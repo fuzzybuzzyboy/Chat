e0f8ac2a39b9bbc360d5ea5a78fb54893de5bbf7
Fuzzy <102690699+fuzzybuzzyboy@users.noreply.github.com>
2024-02-18 09:28:25 +0200

    Clear command :)

--- Code/Client 1.py
+++ Code/Client 1.py
@@ -1,5 +1,4 @@
-import socket
-import threading
+import socket, threading, os
 
 HOST = '127.0.0.1'
 PORT = 5050
@@ -7,12 +6,17 @@ PORT = 5050
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client_socket.connect((HOST, PORT))
 
+print('Connected with success. Prefix is !\n!clear - Clears the chat (your messages and the other client.)\n')
 def send_message():
     while True:
         message = input("Enter message: ")
-        client_socket.sendall(message.encode('utf-8'))
-        print(f'You: {message}')
-        print("\033[A                             \033[A")
+        if message.lower() == '!clear':
+            os.system('cls' if os.name == 'nt' else 'clear')
+            print("Chat cleared.")
+        else:
+            print(f'You: {message}')
+            client_socket.sendall(message.encode('utf-8'))
+            print("\033[A                             \033[A")
 
 send_thread = threading.Thread(target=send_message)
 send_thread.start()
