1a37d615e3e0e9706f1ce3e3f04e16dce08bcf79
Fuzzy <102690699+fuzzybuzzyboy@users.noreply.github.com>
2024-02-18 12:04:02 +0200

    Update and rename Client 1.py to Client.py

--- Code/Client.py
+++ Code/Client.py
@@ -1,4 +1,4 @@
-import socket, threading, os
+import socket, threading, os, sys
 
 HOST = '127.0.0.1'
 PORT = 5050
@@ -7,21 +7,25 @@ client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client_socket.connect((HOST, PORT))
 
 print('Connected with success. Prefix is !\n!clear - Clears the chat (your messages and the other client.)\n')
+
 def send_message():
     while True:
         message = input("Enter message: ")
         if message.lower() == '!clear':
             os.system('cls' if os.name == 'nt' else 'clear')
             print("Chat cleared.")
-        else:
-            print(f'You: {message}')
-            client_socket.sendall(message.encode('utf-8'))
-            print("\033[A                             \033[A")
+        client_socket.sendall(message.encode('utf-8'))
+        if message=="": print('Please actually write something.')
+        sys.stdin.read(0)
 
 send_thread = threading.Thread(target=send_message)
 send_thread.start()
 
 while True:
-    message = client_socket.recv(1024).decode('utf-8')
-    print(f'\nThem: {message}')
-    print("Enter message: ", end='', flush=True)
+    try:
+        message = client_socket.recv(1024).decode('utf-8')
+        print(f'\n{message}')
+        print("Enter message: ", end='', flush=True)
+    except ConnectionResetError:
+        print("\nConnection to the server was lost.")
+        break
