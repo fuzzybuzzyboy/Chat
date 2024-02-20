9aa6bed46e6d970afe350cb6d847379293d1b9fd
Fuzzy <102690699+fuzzybuzzyboy@users.noreply.github.com>
2024-02-20 11:23:08 +0200

    Update Client.py
    Update log
    - add !onlineusers command
    - colored text
    - added 
    usernames

--- Code/Client.py
+++ Code/Client.py
@@ -1,31 +1,41 @@
+from colorama import Fore, Style # styles: BRIGHT, NORMAL, DIM. Fore.WHITE, Style.RESET_ALL (EXAMPLE: Fore.GREEN + Style.BRIGHT + "a")
 import socket, threading, os, sys
+def username_creation():
+    while True:
+        username = input("Enter your username: ")
+        if not username: print('Please actually create a username')
+        if len(username)>16: print('Please pick a name under 16 characters')
+        if username and len(username)<16: return username
+username = username_creation()
 
-HOST = '127.0.0.1'
-PORT = 5050
-
-client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+HOST, PORT, client_socket = '127.0.0.1', 5050, socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client_socket.connect((HOST, PORT))
-
-print('Connected with success. Prefix is !\n!clear - Clears the chat (your messages and the other client.)\n')
+print(Style.BRIGHT + Fore.GREEN + 'Connected with success. Prefix is !\n' + Fore.BLUE + Style.NORMAL+'!clear - Clears the chat (your messages and the other client.)\n!exit - Exits the client (works better than ctrl+c for some reason)\n!onlineusers - Tells you how many connected clients are online\n' + Fore.WHITE, Style.RESET_ALL)
 
 def send_message():
     while True:
         message = input("Enter message: ")
         if message.lower() == '!clear':
             os.system('cls' if os.name == 'nt' else 'clear')
-            print("Chat cleared.")
-        client_socket.sendall(message.encode('utf-8'))
+            print(Fore.BLUE + Style.NORMAL + '!clear - Clears the chat (your messages and the other client.)\n!exit - Exits the client (works better than ctrl+c for some reason)\n!onlineusers - Tells you how many connected clients are online\n' + Fore.WHITE, Style.RESET_ALL)
+        if message.lower() == '!exit':
+            client_socket.close()
+            break
+        client_socket.sendall(f'{message}'.encode('utf-8'))
         if message=="": print('Please actually write something.')
         sys.stdin.read(0)
 
 send_thread = threading.Thread(target=send_message)
 send_thread.start()
+client_socket.sendall(username.encode('utf-8'))
 
 while True:
     try:
         message = client_socket.recv(1024).decode('utf-8')
-        print(f'\n{message}')
-        print("Enter message: ", end='', flush=True)
+        if message.startswith('There are currently') and message.endswith('other client(s) online.'): print(f'\n{message}\nEnter message: ', end='', flush=True)
+        else:
+            print(f'\n{message}')
+            print("Enter message: ", end='', flush=True)
     except ConnectionResetError:
         print("\nConnection to the server was lost.")
         break
