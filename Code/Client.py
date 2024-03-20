f4494e483d8cd790c39623b569dcdffddd133831
Fuzzy <102690699+fuzzybuzzyboy@users.noreply.github.com>
2024-03-20 19:12:15 +0200

    i did something here

--- Code/Client.py
+++ Code/Client.py
@@ -1,17 +1,24 @@
 from colorama import Fore, Style # styles: BRIGHT, NORMAL, DIM. Fore.WHITE, Style.RESET_ALL (EXAMPLE: Fore.GREEN + Style.BRIGHT + "a")
 import socket, threading, os, sys, colorama
 colorama.init(autoreset=True)
+nospace=False
 def username_creation():
     while True:
+        global nospace
         username = input(Fore.BLUE + Style.BRIGHT + "Enter your username: " + Fore.WHITE + Style.RESET_ALL)
+        for i in range(len(username)):
+            if username[i] == ' ': nospace=True
+            else: pass
+            if nospace==True: break
         if not username: print(Fore.RED + Style.BRIGHT + 'Please actually create a username.' + Fore.WHITE + Style.RESET_ALL)
-        if len(username)>16 or len(username)<3 or len(username)==3: print(Fore.RED + Style.BRIGHT + 'Please pick a username under 16 characters and over 3 characters.' + Fore.WHITE + Style.RESET_ALL)
-        if username and len(username)<16 and len(username)>3 and len(username)!=3: return username
+        if len(username)>16 or len(username)<3 or len(username)==3: print(Fore.RED + Style.BRIGHT + 'Please create a username under 16 characters and over 3 characters.' + Fore.WHITE + Style.RESET_ALL)
+        if nospace==True: print(Fore.RED + Style.BRIGHT + 'Please create a username that doesn\'t contain any spaces.' + Fore.WHITE + Style.RESET_ALL)
+        if username and len(username)<16 and len(username)>3 and len(username)!=3 and nospace == False:
+            return username
 username = username_creation()
 
 client_socket, messages_sent, command_count = socket.socket(socket.AF_INET, socket.SOCK_STREAM), 0, 0
-try: client_socket.connect(('127.0.0.1', 5050)), print(Style.BRIGHT + Fore.GREEN + 'Your messages are getting logged.\nConnected with success. Prefix is \'!\'. Client IDS go from oldest -> newest\n' + Fore.BLUE + Style.NORMAL+'!clear - Clears the chat (Client side)\n!exit - Exits the client (works better than ctrl+c for some reason)\n!onlineusers - Tells you how many connected clients are online\n!PM (Private message for short)︱Example: !PM [Client ID] [Your message]\n!messagecount (Or !mc) - Tells you how many messages you\'ve sent\n!commandcount (Or !cc) - Tells you how many commands you\'ve ran\n' + Fore.WHITE, Style.RESET_ALL)
-except ConnectionRefusedError: exit('Failed to connect to server. Please make sure the server is running')
+client_socket.connect(('127.0.0.1', 5050)), print(Style.BRIGHT + Fore.GREEN + 'Your messages are getting logged.\nConnected with success. Prefix is \'!\'. Client IDS go from oldest -> newest\n' + Fore.BLUE + Style.NORMAL+'!clear - Clears the chat (Client side)\n!exit - Exits the client (works better than ctrl+c for some reason)\n!onlineusers - Tells you how many connected clients are online\n!PM (Private message for short)︱Example: !PM [Client ID] [Your message]\n!messagecount (Or !mc) - Tells you how many messages you\'ve sent\n!commandcount (Or !cc) - Tells you how many commands you\'ve ran\n' + Fore.WHITE, Style.RESET_ALL)
 def send_message():
     global messages_sent, command_count
     while True:
